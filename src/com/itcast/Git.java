7ce113829b02e9c3e4ff08dc468598ed3d99c7a6
chenlei <82060396@qq.com>
2019-01-24 22:49:40 +0800

    把本地工程添加

---
+++ src/com/itcast/Git.java
@@ -0,0 +1,13 @@
+package com.itcast;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("啦啦啦啦啦啦啦....");
+        System.out.println("啦啦啦啦啦啦啦....");
+        System.out.println("啦啦啦啦啦啦啦....");
+        System.out.println("啦啦啦啦啦啦啦....");
+        System.out.println("啦啦啦啦啦啦啦....");
+        System.out.println("啦啦啦啦啦啦啦....");
+        System.out.println("啦啦啦啦啦啦啦....");
+    }
+}
